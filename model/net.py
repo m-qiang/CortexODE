ac0065116bc7f5972552a7eced0624cfafc2c35d
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-05 11:53:40 +0200

    net

--- model/net.py
+++ model/net.py
@@ -37,7 +37,12 @@ class Unet(nn.Module):
 
 	
 
+
+
+
+ 
     def forward(self, x):
+        
         
 
         x1 = F.leaky_relu(self.conv1(x), 0.2)
@@ -49,30 +54,31 @@ class Unet(nn.Module):
     # pad x with zeros to match the number of features in x4
         x = F.pad(x, (0, 1), mode='constant', value=0)
         x = self.up(x)
-        
+    
+        x = F.interpolate(x, size=x4.size()[2:], mode='trilinear', align_corners=False)
         x = torch.cat([x, x4], dim=1)
         x = F.leaky_relu(self.deconv4(x), 0.2)
         x = self.up(x)
-        
+    
+        x = F.interpolate(x, size=x3.size()[2:], mode='trilinear', align_corners=False)
         x = torch.cat([x, x3], dim=1)
         x = F.leaky_relu(self.deconv3(x), 0.2)
         x = self.up(x)
-        
+    
+        x = F.interpolate(x, size=x2.size()[2:], mode='trilinear', align_corners=False)
         x = torch.cat([x, x2], dim=1)
         x = F.leaky_relu(self.deconv2(x), 0.2)
         x = self.up(x)
-        
+    
+        x = F.interpolate(x, size=x1.size()[2:], mode='trilinear', align_corners=False)
         x = torch.cat([x, x1], dim=1)
         x = F.leaky_relu(self.deconv1(x), 0.2)
 
         x = F.leaky_relu(self.lastconv1(x), 0.2)
         x = self.lastconv2(x)
-        
+    
         return x
-
-
- 
-        
+   
     #def forward(self, x):
 
         #x1 = F.leaky_relu(self.conv1(x), 0.2)
