0e331f01d0334e0b868fc1002135a3684e63b20d
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-07 16:35:57 +0200

    net

--- model/net.py
+++ model/net.py
@@ -34,7 +34,45 @@ class Unet(nn.Module):
         self.lastconv2 = nn.Conv3d(in_channels=16, out_channels=c_out, kernel_size=3,
                                    stride=1, padding=1)
         self.up = nn.Upsample(scale_factor=2, mode='trilinear')
-        
+    
+    def forward(self, x):
+
+        x1 = F.leaky_relu(self.conv1(x), 0.2)
+        print("Taille du tenseur x1: ", x1.size())
+        x2 = F.leaky_relu(self.conv2(x1), 0.2)
+        print("Taille du tenseur x2: ", x2.size())
+        x3 = F.leaky_relu(self.conv3(x2), 0.2)
+        print("Taille du tenseur x3: ", x3.size())
+    
+    # add padding to match tensor sizes
+        x3 = F.pad(x3, (0, 0, 1, 0, 0, 0), mode='constant', value=0)
+    
+        x4 = F.leaky_relu(self.conv4(x3), 0.2)
+        print("Taille du tenseur x4: ", x4.size())
+        x  = F.leaky_relu(self.conv5(x4), 0.2)
+        print("Taille du tenseur x: ", x.size())
+        x  = self.up(x)
+    
+        x = torch.cat([x, x4], dim=1)
+        x = F.leaky_relu(self.deconv4(x), 0.2)
+        x = self.up(x)
+    
+        x = torch.cat([x, x3], dim=1)
+        x = F.leaky_relu(self.deconv3(x), 0.2)
+        x = self.up(x)
+    
+        x = torch.cat([x, x2], dim=1)
+        x = F.leaky_relu(self.deconv2(x), 0.2)
+        x = self.up(x)
+    
+        x = torch.cat([x, x1], dim=1)
+        x = F.leaky_relu(self.deconv1(x), 0.2)
+
+        x = F.leaky_relu(self.lastconv1(x), 0.2)
+        x = self.lastconv2(x)
+
+        return x
+"""  
     def forward(self, x):
 
         x1 = F.leaky_relu(self.conv1(x), 0.2)
@@ -68,7 +106,7 @@ class Unet(nn.Module):
         x = self.lastconv2(x)
 
         return x
-"""
+
         x4 = F.interpolate(x4, size=x.size()[2:], mode='trilinear', align_corners=True)
         x = torch.cat([x, x4], dim=1)
         x = F.leaky_relu(self.deconv4(x), 0.2)
