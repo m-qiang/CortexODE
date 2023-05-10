811e9b7d080ec2237bb175a835d74620f9a675e1
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-10 11:59:17 +0200

    g

--- model/net.py
+++ model/net.py
@@ -4,7 +4,54 @@ import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 
+ 
 
+
+class Unet(nn.Module):
+    def __init__(self, c_in=1, c_out=2):
+        super(Unet, self).__init__()
+
+        self.conv1 = nn.Conv3d(in_channels=c_in, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1)
+        self.conv4 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+        self.conv5 = nn.Conv3d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1)
+
+        self.deconv4 = nn.ConvTranspose3d(in_channels=256, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.deconv3 = nn.ConvTranspose3d(in_channels=128, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.deconv2 = nn.ConvTranspose3d(in_channels=64, out_channels=16, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.deconv1 = nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=3, stride=2, padding=1, output_padding=1)
+
+        self.lastconv1 = nn.Conv3d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.lastconv2 = nn.Conv3d(in_channels=16, out_channels=c_out, kernel_size=3, stride=1, padding=1)
+        self.up = nn.Upsample(scale_factor=2, mode='trilinear')
+
+    def forward(self, x):
+        x1 = F.leaky_relu(self.conv1(x), negative_slope=0.2)
+        x2 = F.leaky_relu(self.conv2(x1), negative_slope=0.2)
+        x3 = F.leaky_relu(self.conv3(x2), negative_slope=0.2)
+        x4 = F.leaky_relu(self.conv4(x3), negative_slope=0.2)
+        x = F.leaky_relu(self.conv5(x4), negative_slope=0.2)
+        x = self.up(x)
+
+        x = torch.cat([x, x4], dim=1)
+        x = F.leaky_relu(self.deconv4(x), negative_slope=0.2)
+        x = self.up(x)
+
+        x = torch.cat([x, x3], dim=1)
+        x = F.leaky_relu(self.deconv3(x), negative_slope=0.2)
+        x = self.up(x)
+
+        x = torch.cat([x, x2], dim=1)
+        x = F.leaky_relu(self.deconv2(x), negative_slope=0.2)
+        x = self.up(x)
+
+        x = torch.cat([x, x1], dim=1)
+        x = F.leaky_relu(self.deconv1(x), negative_slope=0.2)
+
+        x = F.leaky_relu(self.lastconv1(x), 0.2)
+        x = self.lastconv2(x)
+"""
 # segmentation U-Net
 class Unet(nn.Module):
     def __init__(self, c_in=1, c_out=2):
@@ -104,7 +151,7 @@ class Unet(nn.Module):
 
 
 
-
+"""
 
 
 class CortexODE(nn.Module):
