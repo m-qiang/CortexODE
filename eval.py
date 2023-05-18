183777176103f53a5b2c0c84b9251b00ff0dd6a9
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-18 19:15:10 +0200

    got

--- eval.py
+++ eval.py
@@ -152,8 +152,10 @@ if __name__ == '__main__':
             seg_pred = torch.argmax(seg_out, dim=1)[0]
             if surf_hemi == 'lh':
                 seg = (seg_pred == 1).cpu().numpy()  # lh
-                seg = seg[:, 2:-2, :, :]  # Remove padding
+                seg = seg[2:-2, :, :]  # Remove padding
                 seg_img = nib.Nifti1Image(seg.astype(np.uint8), np.eye(4))
+                print(seg_img.shape)
+
                 nib.save(seg_img, 'lh_segmentation.nii.gz')
 
                 
