e8e82a3871af19ba7d7ee02aa2d644a1036ecb8c
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-23 15:38:01 +0200

    e

--- eval.py
+++ eval.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
             if surf_hemi == 'lh':
                 seg = (seg_pred == 1).cpu().numpy()  # lh
                 seg = seg[2:-2, :, :]  # Remove padding
-                seg_img = nib.Nifti1Image(seg.astype(np.uint8), np.eye(4))
+                seg_img = nib.Nifti1Image(seg.astype(np.uint8), affine=None)
         
         # Generate the file name with counter
                 file_name = f'lh_segmentation{counter}.nii.gz'
