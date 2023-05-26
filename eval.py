41fa446a5aad43636a090ac08d711a63f6c7637f
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-26 10:21:26 +0200

    6006

--- eval.py
+++ eval.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
         elif data_name == 'dhcp':
             brain = nib.load(data_dir+subid+'/'+subid+'_T2w.nii.gz')
             brain_arr = brain.get_fdata()
-            brain_arr = (brain_arr /0.4).astype(np.float16)
+            brain_arr = (brain_arr /600).astype(np.float16)
         brain_arr = process_volume(brain_arr, data_name)
         volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
 
