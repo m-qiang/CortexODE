5e79a6b64b7bb05fd6a60e1cebefe4b304f0df6e
salim rami <salimrami@MacBook-Air-de-salim.local>
2023-05-26 13:31:35 +0200

    42

--- eval.py
+++ eval.py
@@ -147,44 +147,9 @@ if __name__ == '__main__':
         elif data_name == 'dhcp':
             brain = nib.load(data_dir+subid+'/'+subid+'_T2w.nii.gz')
             brain_arr = brain.get_fdata()
-            
-            
-            
-            
-        elif data_name == 'dhcp':
-            brain = nib.load(data_dir + subid + '/' + subid + '_T2w.nii.gz')
-            brain_arr = brain.get_fdata()
-
-    # Calculate the range of voxel intensities
-            min_value = np.min(brain_arr)
-            max_value = np.max(brain_arr)
-
-    # Define the desired range for voxel intensities
-            desired_min = 0  # Update with your desired minimum intensity value
-            desired_max = 3300  # Update with your desired maximum intensity value
-
-    # Calculate the scaling factor
-            scaling_factor = (desired_max - desired_min) / (max_value - min_value)
-
-    # Scale the voxel intensities
-            scaled_brain_arr = (brain_arr - min_value) * scaling_factor + desired_min
-
-    # Convert the voxel intensities to the desired data type
-            brain_arr = scaled_brain_arr
-            brain_arr = brain_arr.astype(np.float16)
-
+            brain_arr = (brain_arr /43).astype(np.float16)
         brain_arr = process_volume(brain_arr, data_name)
         volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
-    
-            
-            
-            
-            
-            
-            
-            #brain_arr = (brain_arr /20).astype(np.float16)
-        #brain_arr = process_volume(brain_arr, data_name)
-        #volume_in = torch.Tensor(brain_arr).unsqueeze(0).to(device)
 
         # ------- predict segmentation ------- 
             
